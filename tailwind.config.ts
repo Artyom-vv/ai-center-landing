9e670d53885b06116e3d17d5b98b575680fe75b3
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-03-01 15:20:38 +0300

    typography & spacings

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type {Config} from "tailwindcss";
+
+export default {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./screens/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                "xl": "1440px",
+                "md": "768px",
+                "sm": "360px",
+            },
+            container: {
+                screens: {
+                    "xl": "1440px",
+                    "md": "768px",
+                    "sm": "360px",
+                },
+                center: true,
+                padding: {
+                    "xl": "80px",
+                    "md": "68px",
+                    "sm": "16px",
+                }
+            }
+        },
+    },
+} satisfies Config;
