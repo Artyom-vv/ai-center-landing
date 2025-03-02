7e16a1046b07000d8f096924382e4ec90783e118
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-03-02 15:39:41 +0300

    variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,25 +8,23 @@ export default {
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {
+        screens: {
+            "xl": "1440px",
+            "md": "768px",
+            "sm": "360px",
+        },
+        container: {
             screens: {
                 "xl": "1440px",
                 "md": "768px",
                 "sm": "360px",
             },
-            container: {
-                screens: {
-                    "xl": "1440px",
-                    "md": "768px",
-                    "sm": "360px",
-                },
-                center: true,
-                padding: {
-                    "xl": "80px",
-                    "md": "68px",
-                    "sm": "16px",
-                }
+            center: true,
+            padding: {
+                "xl": "80px",
+                "md": "68px",
+                "sm": "16px",
             }
-        },
+        }
     },
 } satisfies Config;
