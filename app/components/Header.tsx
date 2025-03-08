cba962c36661dbe54c1973e1ce17c1d46550d565
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-03-09 00:41:49 +0300

    #

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,13 +8,13 @@ const Header = () => {
         <header className="py-normal w-full">
             <div className="container flex gap-4 justify-between items-center">
                 <div className="grow">
-                    <a href="#" className="text-body-big font-heading font-medium no-underline">Центр
+                    <a className="text-body-big font-heading font-medium no-underline">Центр
                         ИИ-трансформации</a>
                 </div>
                 <div className="flex gap-1x-large justify-center basis-[632px]">
-                    <a href="#" className="text-body-big no-underline">ИИ для бизнеса</a>
-                    <a href="#" className="text-body-big no-underline">Этапы внедрения</a>
-                    <a href="#" className="text-body-big no-underline">Наши тарифы</a>
+                    <a className="text-body-big no-underline">ИИ для бизнеса</a>
+                    <a className="text-body-big no-underline">Этапы внедрения</a>
+                    <a className="text-body-big no-underline">Наши тарифы</a>
                 </div>
                 <div className="grow">
                     <Button className="ml-auto" leftIcon={<MailAiLine/>}>
