5a25a8fca354382abf8ef298ba4764d9cbf48da7
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-03-12 20:19:23 +0300

    fix grid

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,17 +6,17 @@ const Header = () => {
 
     return (
         <header className="py-normal w-full">
-            <div className="container flex gap-4 justify-between items-center">
-                <div className="grow">
+            <div className="container grid grid-cols-12 gap-4 items-center">
+                <div className="col-span-3">
                     <a className="text-neutral-text-heading select-none text-body-big font-medium no-underline cursor-pointer">Центр
                         ИИ-трансформации</a>
                 </div>
-                <div className="flex gap-1x-large justify-center basis-[632px]">
+                <div className="col-span-6 flex gap-1x-large justify-center">
                     <a className="cursor-pointer select-none text-body-big text-neutral-text-primary active:text-neutral-text-heading no-underline">ИИ для бизнеса</a>
                     <a className="cursor-pointer select-none text-body-big text-neutral-text-primary active:text-neutral-text-heading no-underline">Этапы внедрения</a>
                     <a className="cursor-pointer select-none text-body-big text-neutral-text-primary active:text-neutral-text-heading no-underline">Наши тарифы</a>
                 </div>
-                <div className="grow">
+                <div className="grow col-span-3">
                     <Button className="ml-auto" leftIcon={<MailAiLine/>}>
                         Напишите нам
                     </Button>
