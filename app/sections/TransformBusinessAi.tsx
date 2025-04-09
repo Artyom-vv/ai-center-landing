93cef48fbc6cb741f26998a8a561f64ecc28f97e
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2025-04-09 13:37:31 +0300

    правка текста мобилки

--- app/sections/TransformBusinessAi.tsx
+++ app/sections/TransformBusinessAi.tsx
@@ -6,97 +6,76 @@ import TiltedCards from "@/app/components/TiltedCards";
 import AdvantagePanel from "@/app/components/AdvantagePanel";
 import RotatingCircle from "@/app/components/RotatingCircle";
 
-const TransformBusinessAi = () => {
+const panels = [
+    {
+        title: "Интерактивные чат-робо﻿ты",
+        description: (
+            <>
+                - Работают 24/7<br/>
+                - Автоматизируют общение с клиентами<br/>
+                - Ускоряют обработку запросов<br/>
+                - Повышают качество сервиса<br/>
+                - Легко интегрируются с CRM<br/>
+            </>
+        ),
+        src: "/advantages-panels/1.png"
+    },
+    {
+        title: "ИИ-инструменты  для аналитики",
+        description: (
+            <>
+                - Анализируют большие объемы данных <br/>
+                - Выявляют возможности для роста <br/>
+                - Предотвращают риски <br/>
+                - Суммаризируют данные и готовят отчеты <br/>
+            </>
+        ),
+        src: "/advantages-panels/2.png"
+    },
+    {
+        title: "ИИ-менеджеры  нового поколения",
+        description: (
+            <>
+                - Общаются с каждым клиентом <br/>
+                - Умеют писать первыми <br/>
+                - Отвечают на все вопросы, если не знают ответ, вызывают человека <br/>
+                - Освобождают ресурсы Вашего бизнеса <br/>
+            </>
+        ),
+        src: "/advantages-panels/3.png"
+    }
+];
 
+const TransformBusinessAi = () => {
     return (
         <Section
             className="z-[21]"
-            title={<>
-                Трансформируйте бизнес <br className="md:hidden"/> с ИИ
-            </>}
+            title={<>Трансформируйте бизнес <br className="md:hidden"/> с ИИ</>}
             beforeContent={<RotatingCircle className="size-[402px] absolute -left-[120px] -top-[80px]"/>}
             afterContainer={(
                 <TiltedCards className="relative z-30 md:block hidden">
-                    <AdvantagePanel
-                        className="md:h-[512px]"
-                        title="Интерактивные чат-робо﻿ты"
-                        description={<>
-                            - Работают 24/7<br/>
-                            - Автоматизируют общение с клиентами<br/>
-                            - Ускоряют обработку запросов<br/>
-                            - Повышают качество сервиса<br/>
-                            - Легко интегрируются с CRM<br/>
-                        </>}
-                        src="/advantages-panels/1.png"
-                    />
-                    <AdvantagePanel
-                        className="md:h-[512px]"
-                        title="ИИ-инструменты  для аналитики"
-                        description={<>
-                            - Анализируют большие объемы данных <br/>
-                            - Выявляют возможности для роста <br/>
-                            - Предотвращают риски <br/>
-                            - Суммаризируют данные и готовят отчеты <br/>
-                        </>}
-                        src="/advantages-panels/2.png"
-                    />
-                    <AdvantagePanel
-                        className="md:h-[512px]"
-                        title="ИИ-менеджеры  нового поколения"
-                        description={<>
-                            - Общаются с каждым клиентом <br/>
-                            - Умеют писать первыми <br/>
-                            - Отвечают на все вопросы, если не знают ответ, вызывают человека <br/>
-                            - Освобождают ресурсы Вашего бизнеса <br/>
-                        </>}
-                        src="/advantages-panels/3.png"
-                    />
+                    {panels.map((panel, i) => (
+                        <AdvantagePanel
+                            key={i}
+                            className="md:h-[512px]"
+                            title={panel.title}
+                            description={panel.description}
+                            src={panel.src}
+                        />
+                    ))}
                 </TiltedCards>
             )}
         >
-            {/*<ChipGroup className="self-center">*/}
-            {/*    <Chip size={breakpoint === "xl" || breakpoint === "md" ? 'large' : 'normal'} isDisabled={true}>Повысьте*/}
-            {/*        эффективность</Chip>*/}
-            {/*    <Chip size={breakpoint === "xl" || breakpoint === "md" ? 'large' : 'normal'} isDisabled={true}>Ускорьте*/}
-            {/*        процессы</Chip>*/}
-            {/*    <Chip size={breakpoint === "xl" || breakpoint === "md" ? 'large' : 'normal'} isDisabled={true}>Увеличьте*/}
-            {/*        прибыль</Chip>*/}
-            {/*</ChipGroup>*/}
-            <div className=" flex-col max-md:flex hidden gap-large">
-                <AdvantagePanel
-                    className="md:h-[512px]"
-                    title="Интерактивные чат-робо﻿ты"
-                    description={<>
-                        - Работают 24/7<br/>
-                        - Автоматизируют общение с клиентами<br/>
-                        - Ускоряют обработку запросов<br/>
-                        - Повышают качество сервиса<br/>
-                        - Легко интегрируются с CRM<br/>
-                    </>}
-                    src="/advantages-panels/1.png"
-                />
-                <AdvantagePanel
-                    className="md:h-[512px]"
-                    title="Продвинутая анали﻿тика"
-                    description={<>
-                        ИИ-инструменты для аналитики:<br/>
-                        - Анализируют большие объемы данных<br/>
-                        - Выявляют возможности для роста<br/>
-                        - Предотвращают риски<br/>
-                        Принимайте только обоснованные решения<br/>
-                    </>}
-                    src="/advantages-panels/2.png"
-                />
-                <AdvantagePanel
-                    className="md:h-[512px]"
-                    title="Персональные ИИ-менеджеры"
-                    description={<>
-                        Интеллектуальная система поддержки клиентов: <br/>
-                        - Отвечает на все вопросы после ﻿покупки <br/>
-                        - Освобождает ресурсы вашего бизнеса
-                    </>}
-                    src="/advantages-panels/3.png"
-                />
+            <div className="flex-col max-md:flex hidden gap-large">
+                {panels.map((panel, i) => (
+                    <AdvantagePanel
+                        key={i}
+                        className="md:h-[512px]"
+                        title={panel.title}
+                        description={panel.description}
+                        src={panel.src}
+                    />
+                ))}
             </div>
         </Section>
     );
